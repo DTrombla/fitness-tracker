178b130258a3594f96748c3d7a15c4959e138e6a
DTrombla <duncan@trombla.org>
2021-01-10 00:36:18 -0800

    added bulk of code

---
+++ routes/apiRoutes.js
@@ -0,0 +1,61 @@
+
+const db = require("../models");
+//const workoutSeed = require("../seeders/seed")
+ module.exports = app => {
+    app.post("/api/workouts", (req, res) => {
+        console.log(req.body)
+        db.Workout.create(req.body,
+        (err, data) => {
+            if (err) {
+                res.json(err);
+            } else {
+
+                res.json(data);
+            }
+        })
+    }); 
+    app.put("/api/workouts/:id", (req, res) => {
+        console.log(req.body)
+        db.Workout.updateOne({
+            _id:req.params.id 
+        },
+        {$push:{exercises:req.body}},
+        (err, data) => {
+            if (err) {
+                res.json(err);
+            } else {
+                console.log(data)
+                res.json(data);
+            }})
+    }); 
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.aggregate([
+            {
+                $sort: {day: -1}
+            },
+            {
+                $addFields: {totalDuration:{$sum: "$exercises.duration"}}
+            },
+            {$limit: 7}
+        ], (err, data) => {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(data);
+            };
+        });
+    });
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.aggregate([
+            {
+                $addFields: {totalDuration: {$sum: "$exercises.duration"}}
+            }
+        ], (err, data) => {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(data);
+            };
+        });
+    });
+}
